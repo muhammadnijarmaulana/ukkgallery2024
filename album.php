74c7dab6d338aa87131ab6940764b5c06e659ad5
muhammadnijarmaulana <132984676+muhammadnijarmaulana@users.noreply.github.com>
2024-02-15 07:43:45 +0700

    Update album.php

--- album.php
+++ album.php
@@ -13,10 +13,13 @@
     <title>Halaman Album</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        /* Tambahkan gaya kustom di sini jika diperlukan */
+    </style>
 </head>
 <body>
     <div class="container">
-        <h1>Halaman Album</h1>
+        <h1 class="mt-4 mb-4">Halaman Album</h1>
         <p>Selamat datang <b><?=$_SESSION['namalengkap']?></b></p>
         
         <ul class="nav">
@@ -26,50 +29,64 @@
             <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
         </ul>
 
-        <form action="tambah_album.php" method="post">
-            <div class="form-group">
-                <label for="namaalbum">Nama Album</label>
-                <input type="text" class="form-control" id="namaalbum" name="namaalbum">
+        <div class="card mt-4">
+            <div class="card-header">
+                Tambah Album
             </div>
-            <div class="form-group">
-                <label for="deskripsi">Deskripsi</label>
-                <input type="text" class="form-control" id="deskripsi" name="deskripsi">
+            <div class="card-body">
+                <form action="tambah_album.php" method="post">
+                    <div class="form-group">
+                        <label for="namaalbum">Nama Album</label>
+                        <input type="text" class="form-control" id="namaalbum" name="namaalbum" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="deskripsi">Deskripsi</label>
+                        <input type="text" class="form-control" id="deskripsi" name="deskripsi">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Tambah</button>
+                </form>
             </div>
-            <button type="submit" class="btn btn-primary">Tambah</button>
-        </form>
+        </div>
 
-        <table class="table mt-3">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nama</th>
-                    <th>Deskripsi</th>
-                    <th>Tanggal dibuat</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    include "koneksi.php";
-                    $userid=$_SESSION['userid'];
-                    $sql=mysqli_query($conn,"select * from album where userid='$userid'");
-                    while($data=mysqli_fetch_array($sql)){
-                ?>
+        <div class="card mt-4">
+            <div class="card-header">
+                Daftar Album
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td><?=$data['albumid']?></td>
-                            <td><?=$data['namaalbum']?></td>
-                            <td><?=$data['deskripsi']?></td>
-                            <td><?=$data['tanggaldibuat']?></td>
-                            <td>
-                                <a href="hapus_album.php?albumid=<?=$data['albumid']?>" class="btn btn-danger">Hapus</a>
-                                <a href="edit_album.php?albumid=<?=$data['albumid']?>" class="btn btn-warning">Edit</a>
-                            </td>
+                            <th>ID</th>
+                            <th>Nama</th>
+                            <th>Deskripsi</th>
+                            <th>Tanggal dibuat</th>
+                            <th>Aksi</th>
                         </tr>
-                <?php
-                    }
-                ?>
-            </tbody>
-        </table>
+                    </thead>
+                    <tbody>
+                        <?php
+                            include "koneksi.php";
+                            $userid=$_SESSION['userid'];
+                            $sql=mysqli_query($conn,"select * from album where userid='$userid'");
+                            while($data=mysqli_fetch_array($sql)){
+                        ?>
+                                <tr>
+                                    <td><?=$data['albumid']?></td>
+                                    <td><?=$data['namaalbum']?></td>
+                                    <td><?=$data['deskripsi']?></td>
+                                    <td><?=$data['tanggaldibuat']?></td>
+                                    <td>
+                                        <a href="hapus_album.php?albumid=<?=$data['albumid']?>" class="btn btn-danger">Hapus</a>
+                                        <a href="edit_album.php?albumid=<?=$data['albumid']?>" class="btn btn-warning">Edit</a>
+                                    </td>
+                                </tr>
+                        <?php
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
     <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -77,3 +94,4 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+
