eaa7040d32aca3cb62f157f866748d81d0392a83
muhammadnijarmaulana <132984676+muhammadnijarmaulana@users.noreply.github.com>
2024-02-15 07:45:10 +0700

    Update foto.php

--- foto.php
+++ foto.php
@@ -16,8 +16,8 @@
 </head>
 <body>
     <div class="container">
-        <h1>Halaman Foto</h1>
-        <p>Selamat datang <b><?=$_SESSION['namalengkap']?></b></p>
+        <h1 class="mt-4 mb-4">Halaman Foto</h1>
+        <p>Selamat datang <b><?php echo isset($_SESSION['namalengkap']) ? $_SESSION['namalengkap'] : ''; ?></b></p>
         
         <ul class="nav">
             <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
@@ -26,83 +26,97 @@
             <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
         </ul>
 
-        <form action="tambah_foto.php" method="post" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="judulfoto">Judul</label>
-                <input type="text" class="form-control" id="judulfoto" name="judulfoto">
+        <div class="card mt-4">
+            <div class="card-header">
+                Tambah Foto
             </div>
-            <div class="form-group">
-                <label for="deskripsifoto">Deskripsi</label>
-                <input type="text" class="form-control" id="deskripsifoto" name="deskripsifoto">
+            <div class="card-body">
+                <form action="tambah_foto.php" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="judulfoto">Judul</label>
+                        <input type="text" class="form-control" id="judulfoto" name="judulfoto">
+                    </div>
+                    <div class="form-group">
+                        <label for="deskripsifoto">Deskripsi</label>
+                        <input type="text" class="form-control" id="deskripsifoto" name="deskripsifoto">
+                    </div>
+                    <div class="form-group">
+                        <label for="lokasifile">Lokasi File</label>
+                        <input type="file" class="form-control-file" id="lokasifile" name="lokasifile">
+                    </div>
+                    <div class="form-group">
+                        <label for="albumid">Album</label>
+                        <select class="form-control" id="albumid" name="albumid">
+                            <?php
+                                include "koneksi.php";
+                                $userid=$_SESSION['userid'];
+                                $sql=mysqli_query($conn,"select * from album where userid='$userid'");
+                                while($data=mysqli_fetch_array($sql)){
+                            ?>
+                                    <option value="<?php echo $data['albumid']; ?>"><?php echo $data['namaalbum']; ?></option>
+                            <?php
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Tambah</button>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="lokasifile">Lokasi File</label>
-                <input type="file" class="form-control-file" id="lokasifile" name="lokasifile">
-            </div>
-            <div class="form-group">
-                <label for="albumid">Album</label>
-                <select class="form-control" id="albumid" name="albumid">
-                    <?php
-                        include "koneksi.php";
-                        $userid=$_SESSION['userid'];
-                        $sql=mysqli_query($conn,"select * from album where userid='$userid'");
-                        while($data=mysqli_fetch_array($sql)){
-                    ?>
-                            <option value="<?=$data['albumid']?>"><?=$data['namaalbum']?></option>
-                    <?php
-                        }
-                    ?>
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary">Tambah</button>
-        </form>
+        </div>
 
-        <table class="table mt-3">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Judul</th>
-                    <th>Deskripsi</th>
-                    <th>Tanggal Unggah</th>
-                    <th>Lokasi File</th>
-                    <th>Album</th>
-                    <th>Disukai</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    include "koneksi.php";
-                    $userid=$_SESSION['userid'];
-                    $sql=mysqli_query($conn,"select * from foto,album where foto.userid='$userid' and foto.albumid=album.albumid");
-                    while($data=mysqli_fetch_array($sql)){
-                ?>
+        <div class="card mt-4">
+            <div class="card-header">
+                Daftar Foto
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td><?=$data['fotoid']?></td>
-                            <td><?=$data['judulfoto']?></td>
-                            <td><?=$data['deskripsifoto']?></td>
-                            <td><?=$data['tanggalunggah']?></td>
-                            <td>
-                                <img src="gambar/<?=$data['lokasifile']?>" width="200px">
-                            </td>
-                            <td><?=$data['namaalbum']?></td>
-                            <td>
-                                <?php
-                                    $fotoid=$data['fotoid'];
-                                    $sql2=mysqli_query($conn,"select * from likefoto where fotoid='$fotoid'");
-                                    echo mysqli_num_rows($sql2);
-                                ?>
-                            </td>
-                            <td>
-                                <a href="hapus_foto.php?fotoid=<?=$data['fotoid']?>" class="btn btn-danger">Hapus</a>
-                                <a href="edit_foto.php?fotoid=<?=$data['fotoid']?>" class="btn btn-warning">Edit</a>
-                            </td>
+                            <th>ID</th>
+                            <th>Judul</th>
+                            <th>Deskripsi</th>
+                            <th>Tanggal Unggah</th>
+                            <th>Lokasi File</th>
+                            <th>Album</th>
+                            <th>Disukai</th>
+                            <th>Aksi</th>
                         </tr>
-                <?php
-                    }
-                ?>
-            </tbody>
-        </table>
+                    </thead>
+                    <tbody>
+                        <?php
+                            include "koneksi.php";
+                            $userid=$_SESSION['userid'];
+                            $sql=mysqli_query($conn,"select * from foto,album where foto.userid='$userid' and foto.albumid=album.albumid");
+                            while($data=mysqli_fetch_array($sql)){
+                        ?>
+                                <tr>
+                                    <td><?php echo $data['fotoid']; ?></td>
+                                    <td><?php echo $data['judulfoto']; ?></td>
+                                    <td><?php echo $data['deskripsifoto']; ?></td>
+                                    <td><?php echo $data['tanggalunggah']; ?></td>
+                                    <td>
+                                        <img src="gambar/<?php echo $data['lokasifile']; ?>" width="200px">
+                                    </td>
+                                    <td><?php echo $data['namaalbum']; ?></td>
+                                    <td>
+                                        <?php
+                                            $fotoid=$data['fotoid'];
+                                            $sql2=mysqli_query($conn,"select * from likefoto where fotoid='$fotoid'");
+                                            echo mysqli_num_rows($sql2);
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <a href="hapus_foto.php?fotoid=<?php echo $data['fotoid']; ?>" class="btn btn-danger">Hapus</a>
+                                        <a href="edit_foto.php?fotoid=<?php echo $data['fotoid']; ?>" class="btn btn-warning">Edit</a>
+                                    </td>
+                                </tr>
+                        <?php
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
     <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
