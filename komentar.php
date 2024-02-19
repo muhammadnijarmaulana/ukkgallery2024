47802186d9e92e1fb06b5a5faf1337f1f3f0c4f9
muhammadnijarmaulana <132984676+muhammadnijarmaulana@users.noreply.github.com>
2024-02-19 13:49:18 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -2,6 +2,7 @@
     session_start();
     if(!isset($_SESSION['userid'])){
         header("location:login.php");
+        exit(); // Pastikan keluar setelah mengarahkan pengguna ke halaman login
     }
 ?>
 
@@ -18,41 +19,47 @@
         .container {
             margin-top: 50px;
         }
+        .table-responsive {
+            margin-top: 20px;
+        }
     </style>
 </head>
 <body>
     <div class="container">
-        <h1>Halaman Komentar</h1>
-        <p>Selamat datang <b><?=$_SESSION['namalengkap']?></b></p>
+        <h1  class="mt-4 mb-4 text-center">Halaman Komentar</h1>
+        <?php if(isset($_SESSION['namalengkap'])): ?>
+        <p  class="mt-4 mb-4 text-center">Selamat datang <b><?= $_SESSION['namalengkap'] ?></b></p>
+        <?php endif; ?>
         
-        <ul class="nav nav-tabs">
-            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="album.php">Album</a></li>
-            <li class="nav-item"><a class="nav-link" href="foto.php">Foto</a></li>
-            <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
-        </ul>
+        <ul  class="nav nav-pills justify-content-center">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="album.php">Album</a></li>
+                <li class="nav-item"><a class="nav-link" href="foto.php">Foto</a></li>
+                <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+            </ul>
 
         <form action="tambah_komentar.php" method="post">
             <?php
                 include "koneksi.php";
-                $fotoid=$_GET['fotoid'];
-                $sql=mysqli_query($conn,"select * from foto where fotoid='$fotoid'");
-                while($data=mysqli_fetch_array($sql)){
+                if(isset($_GET['fotoid'])) {
+                    $fotoid=$_GET['fotoid'];
+                    $sql=mysqli_query($conn,"select * from foto where fotoid='$fotoid'");
+                    while($data=mysqli_fetch_array($sql)){
             ?>
-            <input type="text" name="fotoid" value="<?=$data['fotoid']?>" hidden>
+            <input type="text" name="fotoid" value="<?= $data['fotoid'] ?>" hidden>
             <div class="table-responsive">
                 <table class="table">
                     <tr>
                         <td>Judul</td>
-                        <td><input type="text" name="judulfoto" value="<?=$data['judulfoto']?>" class="form-control"></td>
+                        <td><input type="text" name="judulfoto" value="<?= $data['judulfoto'] ?>" class="form-control"></td>
                     </tr>
                     <tr>
                         <td>Deskripsi</td>
-                        <td><input type="text" name="deskripsifoto" value="<?=$data['deskripsifoto']?>" class="form-control"></td>
+                        <td><input type="text" name="deskripsifoto" value="<?= $data['deskripsifoto'] ?>" class="form-control"></td>
                     </tr>
                     <tr>
                         <td>Foto</td>
-                        <td><img src="gambar/<?=$data['lokasifile']?>" width="200px"></td>
+                        <td><img src="gambar/<?= $data['lokasifile'] ?>" width="200px"></td>
                     </tr>
                     <tr>
                         <td>Komentar</td>
@@ -65,6 +72,7 @@
                 </table>
             </div>
             <?php
+                    }
                 }
             ?>
         </form>
@@ -87,10 +95,10 @@
                         while($data=mysqli_fetch_array($sql)){
                     ?>
                         <tr>
-                            <td><?=$data['komentarid']?></td>
-                            <td><?=$data['namalengkap']?></td>
-                            <td><?=$data['isikomentar']?></td>
-                            <td><?=$data['tanggalkomentar']?></td>
+                            <td><?= $data['komentarid'] ?></td>
+                            <td><?= $data['namalengkap'] ?></td>
+                            <td><?= $data['isikomentar'] ?></td>
+                            <td><?= $data['tanggalkomentar'] ?></td>
                         </tr>
                     <?php
                         }
@@ -106,4 +114,3 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
-
