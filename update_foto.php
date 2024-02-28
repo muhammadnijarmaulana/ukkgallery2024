ff0c5b2d5d73db18d27db693618ad2e6d8d495b3
muhammadnijarmaulana <132984676+muhammadnijarmaulana@users.noreply.github.com>
2024-02-28 14:37:08 +0700

    Update update_foto.php

--- update_foto.php
+++ update_foto.php
@@ -2,32 +2,33 @@
     include "koneksi.php";
     session_start();
 
-    $judulfoto=$_POST['judulfoto'];
-    $deskripsifoto=$_POST['deskripsifoto'];
-    $albumid=$_POST['albumid'];
+    $judulfoto = $_POST['judulfoto'];
+    $deskripsifoto = $_POST['deskripsifoto'];
+    $albumid = $_POST['albumid'];
+    $fotoid = $_POST['fotoid']; // Anda perlu mendapatkan fotoid dari suatu sumber, misalnya form
 
-    //Jika Upload gambar baru
-    if($_FILES['lokasifile']['name']!=""){
+    // Jika Upload gambar baru
+    if($_FILES['lokasifile']['name'] != ""){
         $rand = rand();
-        $ekstensi =  array('png','jpg','jpeg','gif');
+        $ekstensi = array('png','jpg','jpeg','gif');
         $filename = $_FILES['lokasifile']['name'];
         $ukuran = $_FILES['lokasifile']['size'];
         $ext = pathinfo($filename, PATHINFO_EXTENSION);
         
-        if(!in_array($ext,$ekstensi) ) {
+        if(!in_array($ext, $ekstensi)) {
             header("location:foto.php");
-        }else{
-            if($ukuran < 1044070){		
+        } else {
+            if($ukuran < 1044070){      
                 $xx = $rand.'_'.$filename;
                 move_uploaded_file($_FILES['lokasifile']['tmp_name'], 'gambar/'.$rand.'_'.$filename);
-                mysqli_query($conn, "update foto set judulfoto='$judulfoto',deskripsifoto='$deskripsifoto',lokasifile='$xx',albumid='$albumid'");
+                mysqli_query($conn, "UPDATE foto SET judulfoto='$judulfoto', deskripsifoto='$deskripsifoto', lokasifile='$xx', albumid='$albumid' WHERE fotoid=$fotoid");
                 header("location:foto.php");
-            }else{
+            } else {
                 header("location:foto.php");
             }
         }
-    }else{
-        mysqli_query($conn, "update foto set judulfoto='$judulfoto',deskripsifoto='$deskripsifoto',albumid='$albumid'");
+    } else {
+        mysqli_query($conn, "UPDATE foto SET judulfoto='$judulfoto', deskripsifoto='$deskripsifoto', albumid='$albumid' WHERE fotoid=$fotoid");
         header("location:foto.php");
     }
-?>
+?>  
