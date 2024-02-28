caaf9ba2a711731c338ab7b827785200d83ca101
muhammadnijarmaulana <132984676+muhammadnijarmaulana@users.noreply.github.com>
2024-02-28 14:38:30 +0700

    Update home.php

--- home.php
+++ home.php
@@ -6,59 +6,24 @@
     <title>Halaman Home</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        /* Custom CSS */
-        body {
-            background-color: #f8f9fa;
-            padding-top: 20px;
-        }
-        .container {
-            background-color: #fff;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-            margin-top: 50px;
-        }
-        h1 {
-            color: #333;
-            text-align: center;
-        }
-        p {
-            font-size: 18px;
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .nav {
-            justify-content: center;
-        }
-        .nav-item {
-            margin-right: 10px;
-        }
-        .nav-link {
-            color: #007bff;
-        }
-        .nav-link:hover {
-            color: #0056b3;
-        }
-    </style>
 </head>
 <body>
     <div class="container">
-        <h1 class="mt-5">Halaman Home</h1>
+        <h1  class="mt-4 text-center">Halaman Home</h1>
         <?php
             session_start();
             if(isset($_SESSION['userid']) && isset($_SESSION['namalengkap'])) {
         ?>
-                <p>Selamat datang <b><?=$_SESSION['namalengkap']?></b></p>
+                <p class="mt-4 text-center">Selamat datang <b><?=$_SESSION['namalengkap']?></b></p>
         <?php
             } else {
         ?>
-                <p>Silakan <a href="login.php">login</a> untuk mengakses halaman ini.</p>
+                <p class="mt-4 text-center">Silakan <a href="login.php">login</a> untuk mengakses halaman ini.</p>
         <?php
             }
         ?>
         
-        <ul class="nav">
+        <ul class="nav nav-pills justify-content-center">
             <li class="nav-item">
                 <a class="nav-link" href="index.php">Home</a>
             </li>
